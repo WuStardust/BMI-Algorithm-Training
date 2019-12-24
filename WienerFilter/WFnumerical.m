4d96f0ce8d89744366ff80dec2befc6bb01ce3a1
ShengHui_Wu <770577856@qq.com>
2019-12-24 22:44:55 +0800

    numerical

--- WienerFilter/WFnumerical.m
+++ WienerFilter/WFnumerical.m
@@ -1,42 +1,25 @@
 close all;clear;clc;
-load ./data/au_bin@10Hz_1750s.mat
-x = gpuArray(x);
-x = x - mean(x); % turn to zero mean
-d = gpuArray(d);
-[~, neurons] = size(x); % num of neurons
+load au_bin@10Hz_1750s.mat
 fL = 8; % length of the winer filter
+X = ensemble(x, fL);
+[~, neurons] = size(x); % num of neurons
 
 trainN = 0.8*N;
-trainx = x(1:trainN,:);
-traind = d(1:trainN,:);
-maxIter = 200;
+trainX = X(1:trainN,:);
+traind = d(fL:trainN+fL-1,:);
+maxIter = 500;
 lr = 1e-3; % learning rate
-h = zeros(fL, neurons, 2, maxIter, 'gpuArray'); % initial the filter with zero
-xpre = zeros(trainN,neurons);
-ypre = zeros(trainN,neurons);
-Jx = zeros(1, maxIter);
-Jy = zeros(1, maxIter);
+h = zeros(fL*neurons+1, 2, maxIter); % initial the filter with zero
+J = zeros(maxIter,2);
 
-for n=1:neurons % for each neuron
-    for i=1:maxIter % iter to convege
-        % forward
-        xconv = conv(trainx(:,n), h(:,n,1,i));
-    end
+for i=1:maxIter % iter to convege
+  % forward
+  predict = trainX*h(:,:,i);
+  error = predict - traind;
+  J(i,:) = mean(error.^2);
+  plot(J(:,1));
+  drawnow
+
+  % backward
+  h(:,:,i+1) = h(:,:,i) - lr*trainX'*error/trainN;
 end
-
-% for i=1:maxIter % iter to converge
-%     for n=1:neurons % for each neuron
-%         % forward
-%         xconv = conv(trainx(:,n), h(:,n,1,i));
-%         xpre(:,n,i) = xconv(1:trainN);
-%         yconv = conv(trainx(:,n), h(:,n,2,i));
-%         ypre(:,n,i) = yconv(1:trainN);
-%         ex = xpre(:,n,i) - d(:,1);
-%         ey = ypre(:,n,i) - d(:,2);
-%         Jx(n,i) = mean(ex.^2);
-%         Jy(n,i) = mean(ey.^2);
-% 
-%         % backward
-%         gx = xcorr(ex, x())
-%     end
-% end
