df73606a6f415fc46f6afd5be94f8233affdedfb
ShengHui_Wu <770577856@qq.com>
2020-01-10 23:49:07 +0800

    grid search

--- SVR/svr.m
+++ SVR/svr.m
@@ -10,8 +10,8 @@ testX = x(trainN+1:end,:);
 testD = d(trainN+1:end,:);
 
 mse = 1e10;
-for c=[1 10 50 100]
-  for p=[0.1 1 10 50]
+for c=[0.01 0.1 0.5 0.7 1 2 5 10]
+  for p=[0.01 0.05 0.1 0.3 0.5 0.7 1 1.5 2 2.5 3 4 5]
     model = svmtrain(trainD(:,1), trainX, ['-s 3 -t 0', ' -c ', num2str(c), ' -p ', num2str(p)]);
     [xpredict, accuracy, ~] = svmpredict(testD(:,1), testX, model);
     if (accuracy(2)<mse)
